5cf12d55ee4870acfe9324cc1ccbbe4ef510999b
Bryan Windsor <39394303+taidaid@users.noreply.github.com>
2021-03-07 11:51:01 +0100

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="app"></div>
+  <v-app> </v-app>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+
+  components: {
+    //
+  },
+
+  data: () => ({
+    //
+  })
 };
 </script>
-
-<style></style>
